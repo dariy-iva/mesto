d72daeeaea3f741c19fa7b19144719a651d3bf59
Daria Ivanova <dashann@mail.ru>
2021-09-20 11:11:49 +0300

    refactor/установить вебпак, перенести индекс.js в папку src

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ import {
   buttonOpenPopupAddPostElement,
   buttonSubmitFormEditProfileElement,
   buttonSubmitPopupAddProfileElement
-} from '../utils/constants.js';
-import { initialCardsItems } from '../utils/initialCard.js';
-import { toggleButtonOnActive, toggleButtonOnDisabled } from '../utils/utils.js';
+} from '../script/utils/constants.js';
+import { initialCardsItems } from '../script/utils/initialCard.js';
+import { toggleButtonOnActive, toggleButtonOnDisabled } from '../script/utils/utils.js';
 
-import Card from '../components/Card.js';
-import { validationConfig, FormValidator } from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
+import Card from '../script/components/Card.js';
+import { validationConfig, FormValidator } from '../script/components/FormValidator.js';
+import Section from '../script/components/Section.js';
+import PopupWithImage from '../script/components/PopupWithImage.js';
+import PopupWithForm from '../script/components/PopupWithForm.js';
+import UserInfo from '../script/components/UserInfo.js';
 
 const editProfileFormValidate = new FormValidator(validationConfig, formEditProfileElement);
 const addPostFormValidate = new FormValidator(validationConfig, formAddPostElement);
